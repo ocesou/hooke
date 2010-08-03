8ba1fc14056a5b0951d7739f35c6e9c1ef73aef2
tvrkng <tvrkng@fa9c433f-8a47-0410-9964-01fb5b145072>
2010-08-03 00:14:10 +0000

    Ran update_copyright.py in Mercurial branch.
    Mercurial commit
      changeset:   479:04ef4414b162
      branch:      wtk
      tag:         tip
      user:        W. Trevor King <wking@drexel.edu>
      date:        Mon Aug 02 20:10:15 2010 -0400
      summary:     Ran update_copyright.py

--- hooke/ui/gui/__init__.py
+++ hooke/ui/gui/__init__.py
@@ -1,4 +1,23 @@
-# Copyright
+# Copyright (C) 2008-2010 Fabrizio Benedetti
+#                         Massimo Sandal <devicerandom@gmail.com>
+#                         Rolf Schmidt <rschmidt@alcor.concordia.ca>
+#                         W. Trevor King <wking@drexel.edu>
+#
+# This file is part of Hooke.
+#
+# Hooke is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Hooke is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with Hooke.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 """Defines :class:`GUI` providing a wxWidgets interface to Hooke.
 
